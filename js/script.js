ba2c9d57aac00418661882bf14949c07ecd89547
Jesse White <jesse.white6@gmail.com>
2021-04-09 08:45:39 -0700

    create pizza order contstructor.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function PizzaOrder(pizzaSize, pizzaStyle, pizzaToppings, additionalInstructions) {
+    this.pizzaSize = pizzaSize;
+    this.pizzaStyle = pizzaStyle;
+    this.pizzaToppings = pizzaToppings;
+    this.additionalInstructions = additionalInstructions;
+}
