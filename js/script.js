ed0037831c0f1ff22a3ff221bfc1c983a46d8f06
Jesse White <jesse.white6@gmail.com>
2021-04-09 11:38:13 -0700

    WIP: add text from toppings to add to main object.

--- js/script.js
+++ js/script.js
@@ -8,30 +8,30 @@ function PizzaOrder(pizzaSize, pizzaStyle, pizzaToppings, baptismDiscount) {
 }
 
 PizzaOrder.prototype.pizzaPrice = function () {
-    let price = this.pizzaSize + this.pizzaStyle + this.pizzaToppings * this.baptismDiscount;
+    let price = this.pizzaSize + this.pizzaStyle;
     if ($("#mushrooms").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#olives").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#onions").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#pepperoni").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#pineapple").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#sausage").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#bell-peppers").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#anchovies").is(":checked")) {
-        price += 1
+        price += .45
     }
     if ($("#baptism").is(":checked")) {
         price *= .90
@@ -47,12 +47,11 @@ $(document).ready(function () {
 
         const pizzaSizeOrder = parseFloat($("#pizza-size").val());
         const pizzaStyleOrder = parseFloat($("#pizza-style").val());
-        const pizzaToppingsOrder = parseFloat($("#mushrooms").val());
-        const baptismDiscountOrder = parseFloat($("#baptism").val());
+        const pizzaToppingOrder = $("#mushrooms").val();
+        let fullPrice = new PizzaOrder(pizzaSizeOrder, pizzaStyleOrder, pizzaToppingOrder);
+        const convertPrice = fullPrice.pizzaPrice().toFixed(2);
 
-        let fullPrice = new PizzaOrder(pizzaSizeOrder, pizzaStyleOrder, pizzaToppingsOrder, baptismDiscountOrder);
-
-        const convertPrice = fullPrice.pizzaPrice().toFixed(2)
+        console.log(fullPrice)
 
         $("#final-order").text(`Your final price is: $${convertPrice}`);
     });
